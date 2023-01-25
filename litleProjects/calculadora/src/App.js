2672381f09b4962a1a50f01a773715fe906b845d
Uri005 <nba200595@gmail.com>
2023-01-26 00:10:00 +0100

    calculadora development progress

--- litleProjects/calculadora/src/App.js
+++ litleProjects/calculadora/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/descarga.jpg'
+import Boton from './componenetes/Boton';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>hello world!</h1>
+        <div className='freecodecamp-logo-contenedor'>
+          <img 
+            src={freeCodeCampLogo}
+            className='freecodecamp-logo'
+            alt='Logo de freecodecamp' />
+        </div>
+
+        <div className='contenedor-calculadora'>
+          <div className='fila'>
+            <Boton>1</Boton>
+          </div>
+          <div className='fila'></div>
+          <div className='fila'></div>
+          <div className='fila'></div>
+          <div className='fila'></div>
+        </div>
       </header>
     </div>
   );
